74b3bdf6ecb8faf934880e86dce7cd77b2f65beb
Rafael Rodrigues Marquesi <marquesi.cc@gmail.com>
2022-08-11 10:34:23 -0400

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+
+def get_current_datetime() -> str:
+    """Get the current datetime.
+
+    Returns:
+        str: Current datetime.
+    """
+    return datetime.now().strftime('%d-%m-%Y_%H:%M:%S')
