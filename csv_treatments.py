181e144ae59df5fd02c7906b7c0aa5c1f7603736
Rafael Rodrigues Marquesi <marquesi.cc@gmail.com>
2022-09-22 09:03:31 -0400

    Function parameter formatting and other formatting

--- csv_treatments.py
+++ csv_treatments.py
@@ -4,19 +4,19 @@ import reports
 pd.set_option('float_format', '{:f}'.format)
 
 
-def delete_columns(data_frame: pd.DataFrame, columns_names: list) -> pd.DataFrame:
+def delete_columns(data_frame: pd.DataFrame, delete_columns_names: list) -> pd.DataFrame:
     """Delete columns from the DataFrame, given the columns names, passed as parameters.
 
     Args:
         data_frame (pd.DataFrame): DataFrame to be treated.
-        columns_names (list): Array of strings with columns names to exclude.
+        delete_columns_names (list): Array of strings with columns names to exclude.
 
     Returns:
         pd.DataFrame: A DataFrame with deleted columns.
     """
 
     print('*****INICIO DELETE COLUNAS******')
-    for column in columns_names:
+    for column in delete_columns_names:
         if column in data_frame.columns:
             data_frame.drop(column, axis='columns', inplace=True)
             print('Coluna {} excluída.'.format(column))
@@ -27,12 +27,15 @@ def delete_columns(data_frame: pd.DataFrame, columns_names: list) -> pd.DataFram
     return data_frame
 
 
-def load_data(csv_path: str, columns_names: list = None, number_csv_lines: int = None, dtype_dict: dict = None, parse_dates: list = None) -> pd.DataFrame:
+def load_data(csv_path: str, sep: str = ';', encoding: str = 'latin1', decimal: str = ',', delete_columns_names: list = None, number_csv_lines: int = None, dtype_dict: dict = None, parse_dates: list = None) -> pd.DataFrame:
     """Load the CSV file, given the path, column names, and number of rows to load, passed as parameters.
 
     Args:
         csv_path (str): Path where the CSV file is located.
-        columns_names (list, optional): Array of strings with columns names to exclude. Defaults to None.
+        str (str, optional): Separator of the CSV file. Defaults to ';'.
+        encoding (str, optional): Encoding of the CSV file. Defaults to 'latin1'.
+        decimal (str, optional): Decimal separator of the CSV file. Defaults to ','.
+        delete_columns_names (list, optional): Array of strings with columns names to exclude. Defaults to None.
         number_csv_lines (int, optional): Number of lines that will be loaded from the CSV file. Defaults to None.
         dtype_dict (dict, optional): Dictionary with the types of the columns. Defaults to None.
         parse_dates (list, optional): Array of strings with columns names to parse as dates. Defaults to None.
@@ -43,32 +46,40 @@ def load_data(csv_path: str, columns_names: list = None, number_csv_lines: int =
 
     if number_csv_lines != None:
         temp_data = pd.read_csv(
-            csv_path, sep=';', encoding='latin1', decimal=',', nrows=number_csv_lines, dtype=dtype_dict, parse_dates=parse_dates)
+            csv_path, sep=sep, encoding=encoding, decimal=decimal,
+            nrows=number_csv_lines, dtype=dtype_dict, parse_dates=parse_dates
+        )
     else:
         temp_data = pd.read_csv(
-            csv_path, sep=';', encoding='latin1', decimal=',', dtype=dtype_dict, parse_dates=parse_dates)
+            csv_path, sep=sep, encoding=encoding, decimal=decimal, dtype=dtype_dict, parse_dates=parse_dates
+        )
 
     reports.print_informations(temp_data)
     # Used to print min and max of each column
     # reports.print_min_max_column(temp_data)
 
-    if columns_names != None:
-        temp_data = delete_columns(temp_data, columns_names)
+    if delete_columns_names != None:
+        temp_data = delete_columns(
+            data_frame=temp_data, delete_columns_names=delete_columns_names
+        )
 
     reports.print_informations(temp_data)
 
     return temp_data
 
 
-def generate_new_csv(data_frame: pd.DataFrame, csv_path: str) -> None:
+def generate_new_csv(data_frame: pd.DataFrame, csv_path: str, sep: str = ';', encoding: str = 'latin1', index: bool = False) -> None:
     """Generate a new CSV file, given the DataFrame and the path, passed as parameters.
 
     Args:
         data_frame (pd.DataFrame): DataFrame to be treated.
         csv_path (str): Path where the CSV file will be generated.
+        sep (str, optional): Separator of the CSV file. Defaults to ';'.
+        encoding (str, optional): Encoding of the CSV file. Defaults to 'latin1'.
+        index (bool, optional): If True, the index will be included in the CSV file. Defaults to False.
     """
-    data_frame.to_csv(csv_path, sep=';', encoding='latin1', index=False)
-    print('\nCSV gerado com sucesso.')
+    data_frame.to_csv(csv_path, sep=sep, encoding=encoding, index=index)
+    print('\nCSV gerado com sucesso em: {}.'.format(csv_path))
 
 
 def move_cloumns_last_positions(data_frame: pd.DataFrame, columns_names: list) -> pd.DataFrame:
