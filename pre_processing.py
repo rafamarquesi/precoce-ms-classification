a4455df3652eca73575eccf28177f09ec0b3f8ca
Rafael Rodrigues Marquesi <marquesi.cc@gmail.com>
2022-09-22 14:39:23 -0400

    Implementation function delete columns wih single value

--- pre_processing.py
+++ pre_processing.py
@@ -260,3 +260,31 @@ def detect_outliers(series: pd.Series, whis: float = 1.5) -> pd.Series:
     q75, q25 = np.percentile(series, [75, 25])
     iqr = q75 - q25
     return ~((series - series.median()).abs() <= (whis * iqr))
+
+
+def delete_columns_with_single_value(data_frame: pd.DataFrame, summarize: bool = False) -> pd.DataFrame:
+    """Delete the columns with single value.
+
+    Args:
+        data_frame (pd.DataFrame): DataFrame to be treated.
+        summarize (bool, optional): If True, summarize the number of unique in each column. Defaults to False.
+
+    Returns:
+        pd.DataFrame: A DataFrame with the columns with single value deleted.
+    """
+    print('\n*****INICIO DELETE COLUMNS WITH SINGLE VALUE******')
+    counts = data_frame.nunique()
+
+    if summarize:
+        print(counts)
+
+    to_del = [i for i, v in counts.items() if v == 1]
+
+    if to_del:
+        data_frame.drop(to_del, axis=1, inplace=True)
+        print('>>> Colunas removidas com valor único: {}'.format(to_del))
+    else:
+        print('>>> Nenhuma coluna com valor único encontrada.')
+
+    print('*****FIM DELETE COLUMNS WITH SINGLE VALUE*********')
+    return data_frame
