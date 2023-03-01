f23ae84fe42fc371360e6ad9a9669d90e5c70a9c
Rafael Rodrigues Marquesi <marquesi.cc@gmail.com>
2023-03-01 14:44:50 -0400

    Adjustments in pipeline_target_classificacao_02012023 for new configuration parameters

--- pipeline_target_classificacao_02012023.py
+++ pipeline_target_classificacao_02012023.py
@@ -156,9 +156,16 @@ if __name__ == '__main__':
                 precoce_ms_data_frame[settings.class_column].value_counts())
 
             ##### Grid Search Settings #####
+            # Flag to run the original scikit-learn Grid Search CV or the scikit-learn Tuner Grid Search CV (persisting the objects, results, during the execution of the pipeline).
+            # Wheter True, the Grid Search CV Tuner will be used, otherwise the original scikit-learn Grid Search CV will be used.
+            settings.run_grid_search_cv_tuner = True
+
             # Flag to save the results of each split in the pipeline execution, to be used in a possible new execution,
-            # in case the execution is interrupted. Default is True.
-            # settings.save_results_during_run = False
+            # in case the execution is interrupted.
+            # Used only if run_grid_search_cv_tuner = True and It works only if n_jobs = 1 (don't work in parallel).
+            # If false, the results for already executed parameters will be loaded,
+            # but the results for new executed parameters will not be saved.
+            settings.save_results_during_run = True
 
             # Whether True, the objects persisted in the path_objects_persisted_results_runs will be cleaned before the execution of the pipeline
             settings.new_run = False
